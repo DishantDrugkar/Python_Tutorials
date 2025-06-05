19ae1a51709f2141a2ffd84c57e886016ebca55f
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-06-05 19:40:40 +0530

    Set in python

---
+++ Python/set.py
@@ -0,0 +1,11 @@
+collection = {1,2,3,4,5,5}
+print(collection)
+
+
+# How to Create a Empty set
+
+empty = {}   # This is a dictionary
+print(type(empty))
+
+empty_set = set()  # This is an set
+print(type(empty_set))
