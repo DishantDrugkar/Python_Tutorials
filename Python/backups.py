b1b2048d4db4cbf20e7e1104eabcfbfd581387b3
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-06-10 19:22:55 +0530

    Backups

--- Python/backups.py
+++ Python/backups.py
@@ -4,9 +4,8 @@ import shutil
 
 def backup_files(source, destination):
     today = datetime.date.today()
-    backup_files_name = os.path.join(destination, f"backup_{today}.tar.gz")
-    shutil.make_archive(backup_files_name.replace('.tar.gz', ' '), 'gztar', source)
-
+    backup_file_name = os.path.join(destination, f"backup_{today}")
+    shutil.make_archive(backup_file_name, 'zip', source)
 
 source = "/Users/dishantdrugkar/Documents/Programming/VS CODE/PYTHON DEVELOPMENT"
 destination = "/Users/dishantdrugkar/Documents/Programming/VS CODE/PYTHON DEVELOPMENT/Python/Backups"
